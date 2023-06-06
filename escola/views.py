210ac9c9c338a6dd35cf6030d3748d0d26bced03
Micro <reynanvt@gmail.com>
2023-06-06 01:30:06 -0300

    first commit

---
+++ escola/views.py
@@ -0,0 +1,54 @@
+from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunosSerializer, ListaAlunosMatriculadosSerializer
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+
+    serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Listando todas as matrículas"""
+    queryset = Matricula.objects.all()
+
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matrículas de um aluno ou aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    
+    serializer_class = ListaMatriculasAlunosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos e alunas matrículados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    
+    serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+        
